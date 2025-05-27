30ce8a028a616487c5109c2bfbe87208a26e592f
Ital023 <contadoitalo911@gmail.com>
2025-05-26 23:12:46 -0300

    init: Adicionando arquivos iniciais e realizando conexao MySQL

---
+++ src/main/java/io/github/Ital023/Ecommerce_FBR_MySQL/EcommerceFbrMySqlApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.Ecommerce_FBR_MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFbrMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFbrMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Ital023/Ecommerce_FBR_MySQL/EcommerceFbrMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.Ecommerce_FBR_MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFbrMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
