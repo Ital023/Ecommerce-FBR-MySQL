4c9933cf4af1cbe494526b3d524eae3472177f4a
Ital023 <contadoitalo911@gmail.com>
2025-05-29 22:09:50 -0300

    feat: relacionamento composto OrderEntity -> OrderItemEntity -> OrderItemId -> Product

---
+++ src/main/java/io/github/Ital023/Ecommerce_FBR_MySQL/entities/OrderEntity.java
@@ -0,0 +1,58 @@
+package io.github.Ital023.Ecommerce_FBR_MySQL.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_orders")
+public class OrderEntity {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(name = "total")
+    private BigDecimal total;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @OneToMany(mappedBy = "id.order", cascade = CascadeType.ALL)
+    private List<OrderItemEntity> items;
+
+    public OrderEntity() {
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+}
